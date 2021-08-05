b6800750d745fe4a81eb3e1f4d8ab291e6963988
MMostella <masonmostella@gmail.com>
2021-08-05 15:01:37 -0500

    Step 1 - 3

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,11 @@
     https://api.github.com/users/<your name>
 */
 
+axios.get(`https://api.github.com/users/MMostella`)
+.then(res => {
+  console.log('Hiiii');
+})
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +33,35 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [{object}];
+
+const card = document.createElement('div');
+const imgURL = document.createElement('img');
+const cardInfo = document.createElement('div');
+const usersName = document.createElement('h3');
+const username = document.createElement('p');
+const location = document.createElement('p');
+const profile = document.createElement('p');
+const githubAdd = document.createElement('a');
+const followers = document.createElement('p');
+const following = document.createElement('p');
+const bio = document.createElement('p');
+
+card.appendChild(imgURL);
+card.appendChild(cardInfo);
+cardInfo.appendChild(usersName);
+cardInfo.appendChild(username);
+cardInfo.appendChild(location);
+cardInfo.appendChild(profile);
+cardInfo.appendChild(followers);
+cardInfo.appendChild(following);
+cardInfo.appendChild(bio);
+profile.appendChild(githubAdd);
+
+card.classList.add('card');
+cardInfo.classList.add('card-info');
+usersName.classList.add('name');
+username.classList.add('username');
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
